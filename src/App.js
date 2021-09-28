1692739183bd91698bb27f011d0243ae0d3b2aa0
Natalie <nataliesholakis@gmail.com>
2021-09-28 15:23:15 +1000

    App creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import {
+  Button,
+  TextField,
+  Typography,
+  Paper
+} from '@material-ui/core';
+
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function calculate(operation) {
+    switch (operation) {
+      case '-':
+        setResult(number1 - number2);
+        break;
+      case '*':
+        setResult(number1 * number2);
+        break;
+      case '/':
+        setResult(number1 / number2);
+        break;
+      default:
+        setResult(number1 + number2);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper className="paper">
+        <div className="spacer">
+        <TextField onChange={(event) => setNumber1(Number(event.target.value))}></TextField>
+        <TextField onChange={(event) => setNumber2(Number(event.target.value))}></TextField>
+          </div>
+        <br/>
+        <br/>
+        <div className="spacer">
+          <Button variant="contained" onClick={() => calculate('+')}>+</Button>
+          <Button variant="contained" onClick={() => calculate('-')}>-</Button>
+          <Button variant="contained" onClick={() => calculate('*')}>*</Button>
+          <Button variant="contained" onClick={() => calculate('/')}>/</Button>
+        </div>
+        <br/>
+        <Typography>{result}</Typography>
+      </Paper>
     </div>
   );
 }
