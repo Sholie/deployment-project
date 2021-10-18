57de7a60474aabcb096d1e03e817954c18945d2e
JohnSeymour787 <john.seymour787@gmail.com>
2021-10-18 15:45:02 +1100

    Added test for addition calculation

--- src/App.test.js
+++ src/App.test.js
@@ -24,3 +24,29 @@ test('renders division button', () => {
   const divisionButton = screen.getByText("/");
   expect(divisionButton).toBeInTheDocument();
 });
+
+test('calculate addition', () => {
+  const app = render(<App />);
+  const number1Field = app.getByTestId("number1").querySelector("input");
+  const number2Field = app.getByTestId("number2").querySelector("input");
+  
+  const plusButton = app.getByText("+");
+
+  expect(number1Field).toBeInTheDocument();
+  expect(number2Field).toBeInTheDocument();
+  expect(plusButton).toBeInTheDocument();
+  
+  const value1 = 4;
+  const value2 = 2;
+  
+  fireEvent.change(number1Field, { target: { value: value1 } });
+  fireEvent.change(number2Field, { target: { value: value2 } });
+
+  fireEvent.click(plusButton);
+  
+  const resultField = app.getByTestId("result");
+
+  expect(resultField).toBeInTheDocument();
+
+  expect(resultField.textContent).toBe(String(value1 + value2));
+});
